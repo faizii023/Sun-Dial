f604f7ecdf3169ac85cb2b2e65ae2afea28e4750
Faizan <skfaizan2301@gmail.com>
2022-11-16 17:55:42 +0530

    MOON CODE ADDED

--- moon.js
+++ moon.js
@@ -1,8 +1,9 @@
-const canvas = document.querySelector('#canvas');
+// const canvas = document.querySelector('#canvas');
 var ctx = document.querySelector("canvas").getContext("2d"),
   gr = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height),
   sky = new Image();
-
+var ctxMoon = document.querySelector("canvas").getContext("2d"),
+  moonGr = ctxMoon.createLinearGradient(0, 0, 0, ctxMoon.canvas.height);
 sky.onload = go;
 sky.src = "http://i.stack.imgur.com/qhQhQ.jpg";
 //06:30
@@ -12,7 +13,7 @@ var hour = 06,
 var tala = 37 / 60 + 06; // min/60+ht
 console.log(tala + "min");
 var test = (90 + 180) % 360;
-console.log(test+"Angle");
+console.log(test + "Angle");
 function chng(hr, mns) {
   var tttt = mns / 60 + hr; // min/60+ht
 
@@ -25,22 +26,27 @@ function go() {
   gr.addColorStop(0, "#ffc");
   gr.addColorStop(0.75, "gold");
   gr.addColorStop(1, "orange");
+  moonGr.addColorStop(0, "#cd4e");
+  moonGr.addColorStop(0.75, "#aaa");
+  moonGr.addColorStop(0, "#000");
   var angle = 90;
   var oppositeAngle = (angle + 180) % 360;
   console.log(oppositeAngle + "IN");
   ctx.shadowColor = "#ffa";
+  ctxMoon.shadowColor = "#ffa";
   var date = new Date();
   console.log(date.getHours() + "");
   //h=400 w=500
-  var centerX = ctx.canvas.width * 0.5, // center of arc
-    bottomY = ctx.canvas.height + 16, // let center be a bit below horizon
-    radiusX = ctx.canvas.width * 0.52, // radius, 80% of width in this example
-    radiusY = ctx.canvas.height * 0.8; // radius, 90% of height in this example
+  var centerX = 500 * 0.5, // center of arc
+    bottomY = 400 + 16, // let center be a bit below horizon
+    radiusX = 500 * 0.5, // radius, 80% of width in this example
+    radiusY = 400 * 0.9; // radius, 90% of height in this example
 
   // define sunrise and sunset times (in 24-hour clock, can be fractional)
   var time = 7,
     sunrise = 6.5,
     sunset = 18.35;
+  var nTime = 3.5;
   (function loop() {
     var normTime = getTime();
     // console.log(normTime);
@@ -51,8 +57,17 @@ function go() {
     var hr = date.getHours();
     var mns = date.getMinutes();
     var aa = chng(hr, mns);
- 
-    drawSky(normTime); // draw sky gradient
+    // console.log(time + "SUNSET");
+    var nightTime = getNightTime();
+    var nightAngle = getAngle(nightTime);
+    var oppAngleX = (nightAngle + 180) % 360;
+    var oppAngleY = (nightAngle + 180) % 360;
+    var oppx = centerX + radiusX * Math.cos(oppAngleX); // calcuate point
+    var oppy = bottomY + radiusY * Math.sin(oppAngleY);
+    // drawSky(normTime); // draw sky gradient
+
+    drawMoon(oppx, oppy);
+
     drawSun(x, y); // draw sun at point
     drawTime(); // render time
     requestAnimationFrame(loop); // loop
@@ -65,10 +80,18 @@ function go() {
     var mns = date.getMinutes();
     var aa = chng(hr, mns);
 
-    if (time > 23) time = 0;
+    if (time >= 24) time = 0;
     return (time - sunrise) / (sunset - sunrise);
   }
+  function getNightTime() {
+    nTime += 0.033;
+    var hr = date.getHours();
+    var mns = date.getMinutes();
+    var aa = chng(hr, mns);
 
+    if (nTime >= 24) nTime = 0;
+    return (nTime - sunrise) / (sunset - sunrise);
+  }
   function getAngle(normTime) {
     return Math.PI + Math.PI * normTime;
   }
@@ -81,7 +104,14 @@ function go() {
     ctx.shadowBlur = 20;
     ctx.fill();
   }
- 
+  function drawMoon(x, y) {
+    ctxMoon.beginPath();
+    ctxMoon.moveTo(x + 16, y);
+    ctxMoon.arc(x, y, 16, 0, 6.28);
+    ctxMoon.fillStyle = moonGr;
+    ctxMoon.shadowBlur = 20;
+    ctxMoon.fill();
+  }
 
   function drawTime() {
     ctx.fillStyle = "#fff";
@@ -97,22 +127,3 @@ function go() {
     ctx.drawImage(sky, x, 0, 1, sky.height, 0, 0, w, ctx.canvas.height);
   }
 }
-// if (canvas.getContext) {
-
-//     const ctx = canvas.getContext('2d');
-
-//     ctx.strokeStyle = 'green';
-//     ctx.lineWidth = 2;
-
-//     const x = 40,
-//         y = canvas.height / 2,
-//         space = 10,
-//         radius = 30,
-//         arcCount = 6;
-
-//     for (let i = 0; i < arcCount; i++) {
-//         ctx.beginPath();
-//         ctx.arc(x + i * (radius * 2 + space), y, radius, 0, (i + 1) * (2 * Math.PI) / arcCount, false);
-//         ctx.stroke();
-//     }
-// }
--- test.js
+++ test.js
@@ -97,22 +97,3 @@ function go() {
     ctx.drawImage(sky, x, 0, 1, sky.height, 0, 0, w, ctx.canvas.height);
   }
 }
-// if (canvas.getContext) {
-
-//     const ctx = canvas.getContext('2d');
-
-//     ctx.strokeStyle = 'green';
-//     ctx.lineWidth = 2;
-
-//     const x = 40,
-//         y = canvas.height / 2,
-//         space = 10,
-//         radius = 30,
-//         arcCount = 6;
-
-//     for (let i = 0; i < arcCount; i++) {
-//         ctx.beginPath();
-//         ctx.arc(x + i * (radius * 2 + space), y, radius, 0, (i + 1) * (2 * Math.PI) / arcCount, false);
-//         ctx.stroke();
-//     }
-// }
